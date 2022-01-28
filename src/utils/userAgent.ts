a72dfe7be067358b9af09b922b200f5baa9f4f75
voenkomstiwe <radionpon21@gmail.com>
2022-01-28 17:34:34 +0200

    fix: mr issues

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -3,16 +3,5 @@ import { UAParser } from 'ua-parser-js';
 const parser = new UAParser(window.navigator.userAgent);
 const { type } = parser.getDevice();
 
+export const isMobile = type === 'mobile';
 export const isTablet = type === 'tablet';
-
-const isMobileCheck = () => {
-  const userAgent = navigator.userAgent || navigator.vendor;
-
-  const windows = /windows phone/i.test(userAgent);
-  const android = /android/i.test(userAgent);
-  const iOs = /iPad|iPhone|iPod/.test(userAgent);
-
-  return windows || android || iOs;
-};
-
-export const isMobile = isMobileCheck();
