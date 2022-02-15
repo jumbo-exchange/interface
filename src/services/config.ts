e5ad1762160e858fc9b44a331a6fa0a24798ec5a
voenkomstiwe <radionpon21@gmail.com>
2022-02-15 11:48:27 +0200

    feat: fix const, add google analytics

--- src/services/config.ts
+++ src/services/config.ts
@@ -11,7 +11,7 @@ export default function getConfig(
         helperUrl: 'https://helper.testnet.near.org',
         explorerUrl: 'https://explorer.testnet.near.org',
         indexerUrl: 'https://testnet-indexer.ref-finance.com',
-        contractId: 'jumbo-testnet-v2.solniechniy.testnet',
+        contractId: 'jumbo-testnet-v3.solniechniy.testnet',
         nearAddress: 'wrap.testnet',
         stablePoolId: 0,
       };
