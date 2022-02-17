abd8cff59a2bb1460ed6448f65fd6cd09970053c
voenkomstiwe <radionpon21@gmail.com>
2022-02-17 19:41:16 +0200

    fix: fix tooltip

--- src/pages/Pool/styles.ts
+++ src/pages/Pool/styles.ts
@@ -9,7 +9,6 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   max-width: 736px;
-  white-space: nowrap;
   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
     max-width: 328px;
   `}
