5c3679c1f14cd39704cf282965c043ccb9e21edd
voenkomstiwe <radionpon21@gmail.com>
2022-02-18 11:12:24 +0200

    feat: add no result in pool

--- src/pages/Pool/PoolResult.tsx
+++ src/pages/Pool/PoolResult.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IPool, useStore } from 'store';
 import { FilterPoolsEnum } from 'pages/Pool';
+import { noResult } from 'utils/constants';
 import styled from 'styled-components';
 import Big from 'big.js';
 import PoolCardPlaceholder from 'components/Placeholder/PoolCardPlaceholder';
@@ -12,6 +13,15 @@ const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   align-items: center;
+`;
+
+const NoResult = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.188rem;
+  color: ${({ theme }) => theme.globalWhite};
+  margin: 2rem 0;
 `;
 
 export default function PoolResult(
@@ -45,6 +55,12 @@ export default function PoolResult(
             pool={pool}
           />
         ))}
+        {filteredPools.length === 0
+          && (
+            <NoResult>
+              {noResult.yourLiquidity}
+            </NoResult>
+          )}
       </Wrapper>
     );
   }
@@ -57,6 +73,12 @@ export default function PoolResult(
           pool={pool}
         />
       ))}
+      {poolsArray.length === 0
+        && (
+        <NoResult>
+          {noResult.noResultFound}
+        </NoResult>
+        )}
     </Wrapper>
   );
 }
